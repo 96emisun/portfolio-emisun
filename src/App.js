ff0c19134c83414fdc0ccd92fe191e2df4df78d4
96emisun <emil.suq@gmail.com>
2019-09-18 21:05:15 +0200

    Prepare area for welcome/introduction
    Prepared a space where a welcome message or a presentation can be shown. Currently contains lorem ipsum.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,27 @@ function App() {
           <li>Projects</li>
           <li>Contact</li>
         </ul>
-        <hr />
       </nav>
+
+      <content>
+        <div id="introduction" className="ContentBox">
+          <div className="ContentBoxContent">
+            <span className="ContentBoxTitle">Welcome!</span>
+            <p className="ContentBoxText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi urna lacus, auctor vel dignissim eu, ultricies a tortor.
+              Etiam gravida mauris nulla, in molestie nisl accumsan vitae. Duis et posuere dolor. Aenean bibendum ligula ut mi viverra, feugiat scelerisque nisl pulvinar.
+              Nulla viverra ut nunc eget faucibus. Vivamus facilisis orci vel turpis vehicula, at pharetra ligula porta. In luctus congue felis, sit amet accumsan magna ultrices non.
+              <br/>
+              Vivamus vulputate felis nec commodo euismod. Donec tincidunt nisl non quam fermentum, ut lobortis tortor pretium.
+              Donec varius ultricies tellus, vitae convallis quam facilisis vel. Donec eget facilisis nisl. Duis libero nibh, consequat at libero eget, egestas lobortis velit.
+              Sed viverra tempus tellus eget bibendum. Cras aliquet nibh sed mi mollis suscipit. Phasellus porta, magna nec vehicula cursus, elit quam suscipit libero, at pellentesque orci sem ut sapien.
+              Mauris gravida sem lacus, ut semper metus consequat nec. Nunc elementum tortor id urna volutpat gravida. Vivamus quam erat, semper in sem a, vehicula lacinia sapien.
+              Curabitur ligula dui, efficitur eu turpis non, viverra pretium massa. In auctor mollis mauris sit amet dapibus.
+            </p>
+          </div>
+        </div>
+      </content>
+
     </div>
   );
 }
