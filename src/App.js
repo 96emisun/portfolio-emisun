b60feacd8aa257b1631e3a03577e1dd2c1c716d8
96emisun <emil.suq@gmail.com>
2019-09-19 20:16:56 +0200

    Added alt attribute

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <img src={logo} />
+        <img src={logo} alt="" />
         <ul>
           <li>About Me</li>
           <li>Projects</li>
